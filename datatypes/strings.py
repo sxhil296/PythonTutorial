a80e99077a8eb81e45fe30fbacceb84aca078ed9
Sahil <leviack629@gmail.com>
2024-03-03 19:49:04 +0530

    strings

---
+++ datatypes/strings.py
@@ -0,0 +1,53 @@
+# strings are sequence of characters enclosed within ' ' or " " or """ """
+# strings are also immutable
+
+str1 = 'Hello, World!'
+str2 = "Python Programming"
+str3 = """This is a 
+multiline string."""
+print(str1)
+print(str2)
+print(str3)
+
+
+print(str1[0])
+print(str1[-1])
+print(str1[1:4])
+length = len(str1)
+uppercase = str1.upper()
+lowercase = str1.lower()
+capitalize = str1.capitalize()
+title_case = str1.title()
+concatenated_str=str1+" "+str2
+repeated_str = str1*3
+
+names = "Emir, Levi, Eren"
+print(names)
+print(type(names))
+new_names = names.replace("Eren", "Malik")
+print(new_names)
+position = names.find('Levi') 
+index = names.index("Eren") #raises error if absent
+
+
+names_list = names.split(', ')
+print(names_list)
+
+name_str = ', '.join(names_list)
+print(name_str)
+
+example = "pythonnn"
+print(example.count('n'))
+print(example.startswith('s'))
+print(example.endswith('n'))
+
+
+escaped_str1="Emir's Laptop"
+escaped_str2='Emir\'s Laptop'
+raw_str=r'Emir\n Laptop'
+
+
+
+
+
+
