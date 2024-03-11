2762f19419cfdca64cf9429e8ca71decb1fb225b
Sahil <leviack629@gmail.com>
2024-03-11 23:56:51 +0530

    loops

---
+++ loops/main.py
@@ -0,0 +1,15 @@
+# used to repeat a block of code multiple times
+# two types in python - for loop and while loop
+
+
+# FOR LOOP
+# used to iterate over a sequence (like a list, tuple, string, or range) and execute the same code for each item in the sequence.
+
+for x in range(0,10,2):
+    print(x)
+
+
+fruits = ["apple", "banana", "cherry"]
+
+for fruit in fruits:
+    print(fruit)
