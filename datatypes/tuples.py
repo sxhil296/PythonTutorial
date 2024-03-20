8559c06059a66e5c614dbee68245a2171f2fa4d1
Sahil <leviack629@gmail.com>
2024-03-20 10:41:12 +0530

    tuple and its methods

--- datatypes/tuples.py
+++ datatypes/tuples.py
@@ -2,6 +2,7 @@
 #immutable
 #can store diff data types
 #useful when we want data to remain unchanged
+# duplicity is allowed
 
 my_tuple = (0,1,2,3,4,5)
 my_tuple2 = ('a','b','c','d','e')
@@ -18,6 +19,7 @@ print(my_tuple[0:3])
 print(my_tuple[:4])
 print(my_tuple[:])
 print(my_tuple[-1])
+print(my_tuple[::-1])
 
 # can't change values, the following example will give an error i.e typeError
 # my_tuple[0] = 10
@@ -39,6 +41,13 @@ print(tuple(sorted(random_tuple))) #will return a sorted tuple
 occurrence = my_tuple.count(2)
 find_index = my_tuple.index(3)
 
+del my_tuple
+# print(my_tuple) will raise NameError
+
+# empty tuple
+empty_tuple = ()
+print(empty_tuple)
+print(type(empty_tuple))
 
 
 
