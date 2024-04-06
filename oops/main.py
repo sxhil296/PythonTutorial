64195635e1945fecb252b67c8fbb8a04ad7258bb
Sahil <leviack629@gmail.com>
2024-04-06 23:59:23 +0530

    OOPS

---
+++ oops/main.py
@@ -0,0 +1,5 @@
+# In Object-Oriented Programming (OOP), we organize our code into objects that represent real-world entities. These objects have attributes (data) and methods (functions) that operate on the data. 
+
+# For example, if we have a `Car` object, it could have attributes like `color`, `make`, and `model`, and methods like `start()`, `stop()`, and `drive()`. 
+
+# OOP allows us to model complex systems by breaking them down into smaller, more manageable objects that interact with each other. This approach promotes code reusability, modularity, and easier maintenance.
